cfe3b24a7acae88e9737457d7767603ff2e06d3f
Artem Romanov <mehrunesartem@yandex.ru>
2016-11-17 12:19:48 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+//Author: Artem Romanov
+#include <stdio.h>
+#include <bits/stdc++.h>
+//#define TASK "file"
+
+#define F first
+#define S second
+
+using namespace std;
+typedef long double dbl;
+
+const dbl PI = 3.141592653589793238462643383279502884L;
+const dbl E = 2.718281828459045235360287471352662498L;
+const dbl EPS = 1e-12L;
+
+namespace task {
+
+    int main() {
+        return 0;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(11);
+    cout.setf(ios::fixed);
+#ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#elif defined(TASK)
+    freopen(TASK".in", "r", stdin);
+    freopen(TASK".out", "w", stdout);
+#endif
+    return task::main();
+}
